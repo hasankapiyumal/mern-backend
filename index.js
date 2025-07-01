3fc90acf1b4fd30034398d57c1a5009903545c23
Hasanka Piyumal <119643446+hasankapiyumal@users.noreply.github.com>
2025-07-02 00:19:25 +0530

    update cors  package

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import productRouter from './routes/productRouter.js';
 import orderRouter from './routes/orderRouter.js';
-import cores from 'cores';
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 const mongoUrl =process.env.MONOGO_DB_URI;
-app.use=cores();
+app.use(cors());
 
 mongoose.connect(mongoUrl,{});
 const connection = mongoose.connection;
