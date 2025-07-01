7169ce5d21a06652a5458cc9b79d32fd7a8719f1
Hasanka Piyumal <119643446+hasankapiyumal@users.noreply.github.com>
2025-07-01 23:44:11 +0530

    add cores middleware to the project

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import productRouter from './routes/productRouter.js';
 import orderRouter from './routes/orderRouter.js';
+import cores from 'cores';
 dotenv.config();
 
 const app = express();
 const mongoUrl =process.env.MONOGO_DB_URI;
+app.use=cores();
 
 mongoose.connect(mongoUrl,{});
 const connection = mongoose.connection;
